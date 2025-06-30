3bddb52ca37a2befbe3df4d6192f3b666177d482
Juan Martin Pilon <juanmartin.pilon@gmail.com>
2025-06-29 22:04:14 -0300

    Adding the changes to the repo, adding the branches, setting up everything to start with the first component (#1)
    * Adding the changes to the repo, adding the branches, setting up everything to start with the first component
    
    * Starting with the frontend part, installed react router, pre set basic router for login, cleaned basic structure so it can render something , add basic login form implementation
    
    * Added tailwinds, modified the form component to work properly, added dependecies for tailwinds
    
    * Adding styling to the login part, added color palette, changed for inputs and starting to create handleSubmit func
    
    * Added some submit logic to make the events look right
    
    * Added initial full structure of loginForm, we might need to add more details later, but the core stuff its mainly done for loginForm

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
 import './index.css'
 import App from './App.tsx'
 
+// I didnt have the browser router so it was blowing up, added that :')
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </StrictMode>,
 )
