25892395833ee9ee3f53147b2682bc949fa709b2
JuanmaPilon <juanmartin.pilon@gmail.com>
2025-07-04 14:09:41 -0300

    adding correct routing to login, register and basepage, created registerForm and initialized states of variables that will be used for form

---
+++ frontend/src/pages/RegisterForm.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const RegisterForm = () => {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setUserPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
+
+  return <div></div>;
+};
+
+export default RegisterForm;
